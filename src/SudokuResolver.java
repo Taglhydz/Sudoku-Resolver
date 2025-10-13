c34db68c83a86bf37f3f8287a732ea2ad1cc33ea
Tom Vaillant <tom.vaillant.vt@gmail.com>
2025-10-13 16:54:56 +0200

    amélioration du resolver par une nouvelle technique

--- src/SudokuResolver.java
+++ src/SudokuResolver.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class SudokuResolver implements ISudokuResolver {
@@ -22,6 +23,50 @@ public class SudokuResolver implements ISudokuResolver {
                         
                         if (elim.size() == 1) {
                             sudoku[i][j] = elim.get(0);
+                            changed = true;
+                        }
+                    }
+                }
+            }
+
+            // méthode par carré en enlevant les possibilités grace au lignes et colonnes autour
+            for (int carreX = 0; carreX < 3; carreX++) {
+                for (int carreY = 0; carreY < 3; carreY++) {
+                    int startX = carreX * 3;
+                    int startY = carreY * 3;
+                    
+                    ArrayList<List<Object>> lstValuesPossibles = new ArrayList<List<Object>>();
+                    
+                    // prendre les possibilités dans chaque carré
+                    for (int k = startX; k < startX + 3; k++) {
+                        for (int l = startY; l < startY + 3; l++) {
+                            if (sudoku[k][l] == 0) {
+                                ArrayList<Integer> elim = new ArrayList<Integer>(this.remove(remove(getValuesSquare(k, l, sudoku), getValuesRow(k, sudoku)), getValuesColumn(l, sudoku)));
+                                lstValuesPossibles.add(Arrays.asList(k, l, elim));
+                            }
+                        }
+                    }
+                    
+                    // trouver les valeurs uniques
+                    for (int value = 1; value <= 9; value++) {
+                        int nbTimes = 0;
+                        int posX = -1, posY = -1;
+                        
+                        // compte le nombre de fois qu'apparait la valeur
+                        for (List<Object> entry : lstValuesPossibles) {
+                            @SuppressWarnings("unchecked")
+                            ArrayList<Integer> possibilities = (ArrayList<Integer>) entry.get(2);
+                            
+                            if (possibilities.contains(value)) {
+                                nbTimes++;
+                                posX = (int) entry.get(0);
+                                posY = (int) entry.get(1);
+                            }
+                        }
+                        
+                        // si la valeur apparaît une seule fois, l'ajouter au sudoku
+                        if (nbTimes == 1 && posX != -1 && posY != -1) {
+                            sudoku[posX][posY] = value;
                             changed = true;
                         }
                     }
@@ -92,5 +137,9 @@ public class SudokuResolver implements ISudokuResolver {
         for ( int i = 0; i < 9; i++ )
             listCol.add(tab[i][y]);
         return reverse(listCol);
+    }
+
+    public int findByElim(int x, int y, int[][] tab, ArrayList<Integer> lstValuesPossibles) {
+        return 0;
     }
 }
