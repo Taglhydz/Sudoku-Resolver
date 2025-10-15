f7184ee5e0fc47d7fdb3b8bdaf8e04f013f1996b
Tom Vaillant <tom.vaillant.vt@gmail.com>
2025-10-15 14:05:03 +0200

    restructuration total de résolution en ajoutant le backtracking

--- src/SudokuResolver.java
+++ src/SudokuResolver.java
@@ -9,75 +9,181 @@ public class SudokuResolver implements ISudokuResolver {
     }
 
     public boolean resolve(int[][] sudoku) {
+        return resolveBacktracking(sudoku);
+    }
 
-        while (!finito(sudoku)) {
+    private boolean resolveBacktracking(int[][] sudoku) {
+        // etape 1 : première methode de déduction classique
+        boolean progress = true;
+        while (progress && !finito(sudoku)) {
             sudokuDisplayer.display(sudoku);
-
-            boolean changed = false;
-
-            for (int i = 0; i < 9; i++ ) { // tout parcourir le sudoku(sudoku)
-                for (int j = 0; j < 9; j++) {
-                    if (sudoku[i][j] == 0) { //si la case est vide
-
-                        ArrayList<Integer> elim = new ArrayList<Integer>(this.remove(remove(getValuesSquare(i, j, sudoku), getValuesRow(i, sudoku)), getValuesColumn(j, sudoku)));
-                        
-                        if (elim.size() == 1) {
-                            sudoku[i][j] = elim.get(0);
-                            changed = true;
-                        }
-                    }
-                }
-            }
-
-            // méthode par carré en enlevant les possibilités grace au lignes et colonnes autour
-            for (int carreX = 0; carreX < 3; carreX++) {
-                for (int carreY = 0; carreY < 3; carreY++) {
-                    int startX = carreX * 3;
-                    int startY = carreY * 3;
-                    
-                    ArrayList<List<Object>> lstValuesPossibles = new ArrayList<List<Object>>();
-                    
-                    // prendre les possibilités dans chaque carré
-                    for (int k = startX; k < startX + 3; k++) {
-                        for (int l = startY; l < startY + 3; l++) {
-                            if (sudoku[k][l] == 0) {
-                                ArrayList<Integer> elim = new ArrayList<Integer>(this.remove(remove(getValuesSquare(k, l, sudoku), getValuesRow(k, sudoku)), getValuesColumn(l, sudoku)));
-                                lstValuesPossibles.add(Arrays.asList(k, l, elim));
-                            }
-                        }
-                    }
-                    
-                    // trouver les valeurs uniques
-                    for (int value = 1; value <= 9; value++) {
-                        int nbTimes = 0;
-                        int posX = -1, posY = -1;
-                        
-                        // compte le nombre de fois qu'apparait la valeur
-                        for (List<Object> entry : lstValuesPossibles) {
-                            @SuppressWarnings("unchecked")
-                            ArrayList<Integer> possibilities = (ArrayList<Integer>) entry.get(2);
-                            
-                            if (possibilities.contains(value)) {
-                                nbTimes++;
-                                posX = (int) entry.get(0);
-                                posY = (int) entry.get(1);
-                            }
-                        }
-                        
-                        // si la valeur apparaît une seule fois, l'ajouter au sudoku
-                        if (nbTimes == 1 && posX != -1 && posY != -1) {
-                            sudoku[posX][posY] = value;
-                            changed = true;
-                        }
-                    }
-                }
-            }
-
-            if (!changed) break;
+            progress = firstMethodDeduction(sudoku);
         }
 
-        sudokuDisplayer.display(sudoku);
-        return finito(sudoku);
+        // sudoku résolu
+        if (finito(sudoku)) {
+            sudokuDisplayer.display(sudoku);
+            return true;
+        }
+
+        // sudoku avec erreur
+        if (hasError(sudoku)) {
+            return false;
+        }
+
+        // etape 2: backtracking si bloqué
+        // trouve la case possibilités min
+        int minPossibilities = 10;
+        int bestX = -1, bestY = -1;
+        ArrayList<Integer> bestPossibilities = null;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sudoku[i][j] == 0) {
+                    ArrayList<Integer> possibilities = this.remove(
+                        remove(getValuesSquare(i, j, sudoku), getValuesRow(i, sudoku)), 
+                        getValuesColumn(j, sudoku)
+                    );
+                    
+                    if (possibilities.size() == 0) {
+                        // pas de possibilité -> erreur
+                        return false;
+                    }
+                    
+                    if (possibilities.size() < minPossibilities) {
+                        minPossibilities = possibilities.size();
+                        bestX = i;
+                        bestY = j;
+                        bestPossibilities = possibilities;
+                    }
+                }
+            }
+        }
+
+        // pas de case vide trouvé -> fini
+        if (bestX == -1) {
+            sudokuDisplayer.display(sudoku);
+            return finito(sudoku);
+        }
+
+        // essaye chaque possibilité pour la case choisie au dessus
+        for (int value : bestPossibilities) {
+            // créé une copie du sudoku pour test
+            int[][] sudokuCopy = copySudoku(sudoku);
+            sudokuCopy[bestX][bestY] = value;
+
+            // essaye de résoudre avec une valeur
+            if (resolveBacktracking(sudokuCopy)) {
+                // si résolu -> copie de la copie vers l'original
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        sudoku[i][j] = sudokuCopy[i][j];
+                    }
+                }
+                return true;
+            }
+        }
+
+        // aucune possibilité fonctionné -> échec
+        return false;
+    }
+
+    private boolean firstMethodDeduction(int[][] sudoku) {
+        boolean changed = false;
+
+        // 1ere méthode : case avec une possibilité
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sudoku[i][j] == 0) {
+                    ArrayList<Integer> elim = new ArrayList<Integer>(
+                        this.remove(remove(getValuesSquare(i, j, sudoku), getValuesRow(i, sudoku)), 
+                        getValuesColumn(j, sudoku))
+                    );
+                    
+                    if (elim.size() == 1) {
+                        sudoku[i][j] = elim.get(0);
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        // 2eme méthode : val uniques dans un carré
+        for (int carreX = 0; carreX < 3; carreX++) {
+            for (int carreY = 0; carreY < 3; carreY++) {
+                int startX = carreX * 3;
+                int startY = carreY * 3;
+                
+                ArrayList<List<Object>> lstValuesPossibles = new ArrayList<List<Object>>();
+                
+                // prendre les possibilité de chaque case du carré
+                for (int k = startX; k < startX + 3; k++) {
+                    for (int l = startY; l < startY + 3; l++) {
+                        if (sudoku[k][l] == 0) {
+                            ArrayList<Integer> elim = new ArrayList<Integer>(
+                                this.remove(remove(getValuesSquare(k, l, sudoku), getValuesRow(k, sudoku)), 
+                                getValuesColumn(l, sudoku))
+                            );
+                            lstValuesPossibles.add(Arrays.asList(k, l, elim));
+                        }
+                    }
+                }
+                
+                // trouver les valeurs uniques
+                for (int value = 1; value <= 9; value++) {
+                    int nbTimes = 0;
+                    int posX = -1, posY = -1;
+                    
+                    // counter
+                    for (List<Object> entry : lstValuesPossibles) {
+                        @SuppressWarnings("unchecked")
+                        ArrayList<Integer> possibilities = (ArrayList<Integer>) entry.get(2);
+                        
+                        if (possibilities.contains(value)) {
+                            nbTimes++;
+                            posX = (int) entry.get(0);
+                            posY = (int) entry.get(1);
+                        }
+                    }
+                    
+                    // si une seule occurence -> ajouter au sudoku
+                    if (nbTimes == 1 && posX != -1 && posY != -1) {
+                        sudoku[posX][posY] = value;
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        return changed;
+    }
+
+    private boolean hasError(int[][] sudoku) { //fini
+        // si une case vide sans aucune possibilité alors il y a une erreur
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sudoku[i][j] == 0) {
+                    ArrayList<Integer> possibilities = this.remove(
+                        remove(getValuesSquare(i, j, sudoku), getValuesRow(i, sudoku)), 
+                        getValuesColumn(j, sudoku)
+                    );
+                    if (possibilities.size() == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private int[][] copySudoku(int[][] sudoku) { //fini
+        int[][] copy = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                copy[i][j] = sudoku[i][j];
+            }
+        }
+        return copy;
     }
 
     public ArrayList<Integer> remove(ArrayList<Integer> a, ArrayList<Integer> b) { //fini
